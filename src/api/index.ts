f3e7fd87b0974ca48b803eedafad5dd66d340a5b
oiceo123 <iceome44@gmail.com>
2024-01-07 02:23:59 +0700

    ✨ สร้าง axios instance

---
+++ src/api/index.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    const apiKey = import.meta.env.VITE_API_KEY;
+    const access_token = localStorage.getItem("access_token");
+    if (apiKey) {
+      config.headers["X-Api-Key"] = apiKey;
+    }
+    if (access_token) {
+      config.headers["Authorization"] = `Bearer ${access_token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
