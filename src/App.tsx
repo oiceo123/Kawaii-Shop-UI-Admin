071c4932882a2f6ab76cba37c67a57388db130f5
oiceo123 <iceome44@gmail.com>
2024-01-07 02:30:22 +0700

    ✨ สร้าง router guard, auth route

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import React from "react";
 import Router from "./routes";
-import { Layout } from "antd";
-import SidebarContainer from "./containers/Sidebar";
-
-const { Content } = Layout;
 
 const App: React.FC = () => {
   return (
     <>
-      <Layout hasSider>
-        <SidebarContainer />
-        <Content style={{ margin: "16px" }}>
-          <Router />
-        </Content>
-      </Layout>
+      <Router />
     </>
   );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,45 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Switch, Route, useHistory } from "react-router-dom";
+import { useAppSelector } from "../redux";
+
+import { Layout } from "antd";
 import Home from "../pages/Home";
+import SignIn from "../pages/SignIn";
+import SidebarContainer from "../containers/Sidebar";
+
+const { Content } = Layout;
 
 const Router: React.FC = () => {
+  const history = useHistory();
+  const { user } = useAppSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (user) {
+      history.push("/");
+    } else {
+      history.push("signin");
+    }
+  }, [user, history]);
+
+  if (user) {
+    return (
+      <Layout hasSider>
+        <SidebarContainer />
+        <Content style={{ margin: "16px" }}>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+          </Switch>
+        </Content>
+      </Layout>
+    );
+  }
+
   return (
     <Switch>
-      <Route path="/" exact>
-        <Home />
+      <Route path="/signin" exact>
+        <SignIn />
       </Route>
     </Switch>
   );
