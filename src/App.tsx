f2d2a20c17918aff20115401e2c73d64ddd4229b
oiceo123 <iceome44@gmail.com>
2024-01-16 02:09:35 +0700

    🔥 ลบโค้ดที่ไม่จำเป็น

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Router from "./routes";
+import axios from "./api";
+import { useAppDispatch } from "./redux";
+import { setUser, logout, setUserLoading } from "./redux/slices/authSlice";
+
+import Swal from "sweetalert2";
 
 const App: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  const fetchUser = async () => {
+    try {
+      const uid = localStorage.getItem("uid");
+      if (uid) {
+        const res = await axios(`/users/${uid}`);
+        if (res.data) {
+          dispatch(setUser(res.data));
+        }
+      }
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        text: "Session has expired. Please log in again.",
+      });
+      localStorage.removeItem("uid");
+      localStorage.removeItem("oid");
+      localStorage.removeItem("access_token");
+      localStorage.removeItem("refresh_token");
+      dispatch(logout());
+    } finally {
+      dispatch(setUserLoading(false));
+    }
+  };
+
   return (
     <>
       <Router />
