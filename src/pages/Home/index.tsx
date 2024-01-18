19df4fa6ec91a307ba239b51d6290d838c11deb9
oiceo123 <iceome44@gmail.com>
2024-01-18 17:10:35 +0700

    🔥 ลบ Pagination 💄 เปลี่ยน layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,29 +1,28 @@
 import React from "react";
 
 import "./Home.scss";
-import { Col, Row, Pagination } from "antd";
+import { Col, Row } from "antd";
 import InputSearchContainer from "../../containers/InputSearch";
 import ProductContainer from "../../containers/Product";
+import SelectSortContainer from "../../containers/SelectSort";
 
 const Home: React.FC = () => {
   return (
     <>
       <Row className="web-pages-home-input-search-contianer">
-        <Col span={6}>
+        <Col span={5}>
           <h1>Products</h1>
         </Col>
-        <Col offset={12} span={6}>
+        <Col span={13} style={{ textAlign: "center" }}>
           <InputSearchContainer />
+        </Col>
+        <Col span={6} style={{ textAlign: "right" }}>
+          <SelectSortContainer />
         </Col>
       </Row>
       <Row gutter={[16, 16]}>
         <ProductContainer />
       </Row>
-      <Pagination
-        className="web-pages-home-pagination-container"
-        defaultCurrent={6}
-        total={500}
-      />
     </>
   );
 };
