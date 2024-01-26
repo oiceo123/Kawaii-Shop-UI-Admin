a9024e6dd4653533df48f4a7c5ebc5a5abb1d119
oiceo123 <iceome44@gmail.com>
2024-01-27 02:14:21 +0700

    ♻️ เปลี่ยนที่ import User type

--- src/redux/slices/authSlice/index.ts
+++ src/redux/slices/authSlice/index.ts
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
-import type { User } from "./types";
+import type { User } from "../../../types/User";
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 type authState = {
